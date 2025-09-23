f5a6d66368ebd11cfc90659696a9b10c6c4b1f54
Karasik-NN <nikita.nikodimovs@sk.lvt.lv>
2025-09-23 11:48:18 +0300

    pabeigta klase aplis

--- src/oop_figuras/Aplis.java
+++ src/oop_figuras/Aplis.java
@@ -1,5 +1,29 @@
 package oop_figuras;
 
 public class Aplis {
+public Centrs punkts;
+public double radiuss;
 
+public Aplis (Centrs p, double r) {
+	punkts =p;
+	radiuss =r;
 }
+
+public String izvaditApliInfo() {
+	return "Radiuss: "+radiuss;
+}
+
+//izveidot metodi rinka linijas prrekinosana
+public double rLGarums() {
+	double c = 2*Math.PI*radiuss;
+	return Math.round(c*100.0)/100.0;
+}
+//rinka laukuma aprekinosanai
+public double rLaukums() {
+	double s = Math.PI*Math.pow(radiuss, 2);
+	return Math.round(s*100.0)/100.0;
+}
+}
+
+
+
